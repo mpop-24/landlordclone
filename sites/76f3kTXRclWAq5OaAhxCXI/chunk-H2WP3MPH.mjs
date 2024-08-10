dbdf257f5e855a416deefcb02effee53aec03233
Markell Popov <markellpopov@markells-mac.lan>
2024-08-09 23:08:57 -0400

    added content

---
+++ sites/76f3kTXRclWAq5OaAhxCXI/chunk-H2WP3MPH.mjs
@@ -0,0 +1,2 @@
+function t(a,e){return{description:"Welcome to Landlord Ronald Meme Token! \u{1F680}",favicon:"assets/vTZrGx2LR9wP5BpSK1x3fkYD7m4.avif",robots:"max-image-preview:large",socialImage:"assets/EPkF9XRvoqyOZKqEMCZqy5IuQ2A.avif",title:"$ LANDLORD RONALD"}}export{t as a};
+//# sourceMappingURL=chunk-H2WP3MPH.mjs.map
